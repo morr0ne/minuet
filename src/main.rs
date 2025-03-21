9d4d3c121e5fecb6798f0ddc5c175ae4180cd7d6
Matilde Morrone <contact@morrone.dev>
2025-03-21 18:00:40 +0100

    Simplify slint boilerplate

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,9 @@ use anyhow::Result;
 slint::include_modules!();
 
 fn main() -> Result<()> {
-    let ui = AppWindow::new()?;
+    let main_window = MainWindow::new()?;
 
-    ui.on_request_increase_value({
-        let ui_handle = ui.as_weak();
-        move || {
-            let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
-        }
-    });
-
-    ui.run()?;
+    main_window.run()?;
 
     Ok(())
 }
